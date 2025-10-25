6d548cfce6a027c9abbb4bc6e2de1c7893fb9cef
gusfloresok <90418192+gusfloresok@users.noreply.github.com>
2025-10-25 15:23:38 -0300

    Impementacion del nuevo root, fix actualizacion de docker

--- quantix-frontend/src/app/router.tsx
+++ quantix-frontend/src/app/router.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import { LoginPage } from "../features/auth/pages/LoginPage";
-import { DashboardPage } from "./layout/DashboardPage";
+import DashboardPage from "./layout/DashboardPage";
 import { PrivateRoute } from "./PrivateRoute";
 import { MovementsPage } from "../features/movements/pages/MovementsPage";
+import Home from "../Pages/Home";
+import Products from "../Pages/Products";
+import Stock from "../Pages/Stock";
+import StockMov from "../Pages/StockMov";
 
 export const router = createBrowserRouter([
   { path: "/", element: <LoginPage /> },
+
   {
     path: "/dashboard",
     element: (
@@ -13,7 +18,26 @@ export const router = createBrowserRouter([
         <DashboardPage />
       </PrivateRoute>
     ),
+    children: [
+      {
+        path: "home", 
+        element: <Home />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "stock",
+        element: <Stock />,
+      },
+      {
+        path: "movements",
+        element: <StockMov />,
+      },
+    ],
   },
+
   {
     path: "/movements",
     element: (
--- quantix-frontend/src/main.tsx
+++ quantix-frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./app/App";
+import "./styles/global.css";
 
 // Estilos globales
 import "bootstrap/dist/css/bootstrap.min.css";
