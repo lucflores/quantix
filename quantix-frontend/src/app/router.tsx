b6084dcb0b358d9543315dc997579e5835ee21e8
gusfloresok <90418192+gusfloresok@users.noreply.github.com>
2025-11-04 15:44:58 -0300

    Nuevo apartado de cuenta corriente, sin conexion al back

--- quantix-frontend/src/app/router.tsx
+++ quantix-frontend/src/app/router.tsx
@@ -9,6 +9,9 @@ import StockMov from "../features/movements/pages/StockMov";
 import NewProduct from "../features/products/Components/NewProduct";
 import TransactionRegister from "../features/TransactionRegister/Pages/TransactionRegister";
 import NewTransaction from "../features/TransactionRegister/Pages/NewTransaction";
+import PartnersList from "../features/partners/pages/PartnersList";
+import CuentaCte from "../features/partners/pages/CuentaCte";
+import NewBalance from "../features/partners/pages/NewBalance";
 
 export const router = createBrowserRouter([
   { path: "/", element: <LoginPage /> },
@@ -27,6 +30,9 @@ export const router = createBrowserRouter([
       { path: "products/new", element: <NewProduct /> },
       { path: "stock", element: <Stock /> },
       { path: "movements", element: <StockMov /> },
+      { path: "partners", element: <PartnersList /> },
+      { path: "partners/cuenta", element: <CuentaCte /> },
+      { path: "partners/cuenta/new", element: <NewBalance /> },
     ],
   },
 
